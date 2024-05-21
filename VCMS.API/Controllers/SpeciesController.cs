36f819e54cde17283be150fde2257fdfb7c17d41
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-05-21 20:18:01 +0300

    Update SpeciesController

--- VCMS.API/Controllers/SpeciesController.cs
+++ VCMS.API/Controllers/SpeciesController.cs
@@ -19,13 +19,12 @@
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetSpeciesById(int id)
         {
-            var species = await _speciesService.GetByIdAsync(id);
-            return StatusCode((int)species.StatusCode, species);
+            var response = await _speciesService.GetSpeciesByIdAsync(id);
+            return StatusCode((int)response.StatusCode, response);
         }
 
 
-
-        [SwaggerOperation(Summary = "Get species by Id",
+        [SwaggerOperation(Summary = "Get all species",
             Description = "Get species by Id from database.")]
         [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status404NotFound)]
@@ -34,20 +33,19 @@
         [HttpGet("[action]")]
         public async Task<IActionResult> GetAllSpecies()
         {
-            var species = await _speciesService.GetAllAsync();
-            return StatusCode((int)species.StatusCode, species);
+            var response = await _speciesService.GetAllSpeciesAsync();
+            return StatusCode((int)response.StatusCode, response);
         }
-
-
 
 
         [SwaggerOperation(Summary = "Create a species.",
             Description = "Create a species in the database.")]
         [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status500InternalServerError)]
         [ResponseCache(CacheProfileName = "NoCache")]
-        [HttpPost("[action]")]
+        [HttpPost]
         public async Task<IActionResult> CreateSpecies(SpeciesDto speciesDto)
         {
             if (!ModelState.IsValid)
@@ -55,8 +53,41 @@
                 return BadRequest(ModelState);
             }
 
-            var species = await _speciesService.CreateAsync(speciesDto);
-            return StatusCode((int)species.StatusCode, species);
+            var response = await _speciesService.CreateSpeciesAsync(speciesDto);
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+
+        [SwaggerOperation(Summary = "Update a species by Id",
+            Description = "Update a species by Id and name sent in the request body.")]
+        [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status500InternalServerError)]
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpeciesById(int id, SpeciesDto speciesDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var response = await _speciesService.UpdateSpeciesByIdAsync(id, speciesDto);
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+
+        [SwaggerOperation(Summary = "Delete a species with Id", 
+            Description = "Delete a species with Id from database.")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Response<SpeciesDto>), StatusCodes.Status500InternalServerError)]
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteSpeciesById(int id)
+        {
+            var response = await _speciesService.DeleteSpeciesByIdAsync(id);
+            return StatusCode((int)response.StatusCode, response);
         }
     }
 }
