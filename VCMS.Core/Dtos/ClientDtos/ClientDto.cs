4cd294ff9a533fdbb2dc6eb536b381f4022dd352
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-05-25 18:26:10 +0300

    feat: Add Dtos folder with ClientDtos folder, and DiagnosisDtos folder
    - Added ClientDto class for client data transfer
    - Added DiagnosisDto class for diagnosis data transfer

---
+++ VCMS.Core/Dtos/ClientDtos/ClientDto.cs
@@ -0,0 +1,33 @@
+﻿namespace VCMS.Core.Dtos.ClientDtos
+{
+    public class ClientDto
+    {
+        /// <summary>
+        /// Client's First Name
+        /// </summary>
+        [Required(ErrorMessage = "FirstName is required.")]
+        [MaxLength(50, ErrorMessage = "FirstName can't be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Client's Last Name
+        /// </summary>
+        [Required(ErrorMessage = "LastName is required.")]
+        [MaxLength(50, ErrorMessage = "LastName can't be longer than 50 characters.")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Client's Address
+        /// </summary>
+        [Required(ErrorMessage = "Address is required.")]
+        [MaxLength(100, ErrorMessage = "Address can't be longer than 100 characters.")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Client's Phone Number
+        /// </summary>
+        [Required(ErrorMessage = "PhoneNumber is required.")]
+        [MaxLength(15, ErrorMessage = "PhoneNumber can't be longer than 15 characters.")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ VCMS.Core/Dtos/DiagnosisDtos/DiagnosisDto.cs
@@ -0,0 +1,12 @@
+﻿namespace VCMS.Core.Dtos.DiagnosisDtos
+{
+    public class DiagnosisDto
+    {
+        /// <summary>
+        /// Name of Diagnosis
+        /// </summary>
+        [Required(ErrorMessage = "Name is required.")]
+        [MaxLength(50, ErrorMessage = "Name can't be longer than 50 characters.")]
+        public string Name { get; set; }
+    }
+}
