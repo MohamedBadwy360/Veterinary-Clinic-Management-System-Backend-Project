3e05d92c5dad3ab60b3053ee5f9bb1070228fad0
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-05-25 18:31:05 +0300

    feat: Add IDiagnosisService interface to define diagnosis handling contract

---
+++ VCMS.Core/Interfaces/ServicesInterfaces/IDiagnosisService.cs
@@ -0,0 +1,11 @@
+﻿namespace VCMS.Core.Interfaces.ServicesInterfaces
+{
+    public interface IDiagnosisService
+    {
+        Task<Response<DiagnosisDto>> GetDiagnosisByIdAsync(int id);
+        Task<Response<IEnumerable<DiagnosisDto>>> GetAllDiagnosisAsync();
+        Task<Response<DiagnosisDto>> CreateDiagnosisAsync(DiagnosisDto diagnosisDto);
+        Task<Response<DiagnosisDto>> UpdateDiagnosisByIdAsync(int id, DiagnosisDto diagnosisDto);
+        Task<Response<DiagnosisDto>> DeleteDiagnosisByIdAsync(int id);
+    }
+}
