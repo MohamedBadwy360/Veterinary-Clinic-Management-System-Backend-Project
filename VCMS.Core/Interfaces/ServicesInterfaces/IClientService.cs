9245a10ffe1de6cf83ac876324405a7b6d609274
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-05-25 18:34:04 +0300

    feat: Add IClientService interface to define client handling contract

---
+++ VCMS.Core/Interfaces/ServicesInterfaces/IClientService.cs
@@ -0,0 +1,11 @@
+﻿namespace VCMS.Core.Interfaces.ServicesInterfaces
+{
+    public interface IClientService
+    {
+        Task<Response<ClientDto>> GetClientByIdAsync(int id);
+        Task<Response<IEnumerable<ClientDto>>> GetAllClientAsync();
+        Task<Response<ClientDto>> CreateClientAsync(ClientDto clientDto);
+        Task<Response<ClientDto>> UpdateClientByIdAsync(int id, ClientDto clientDto);
+        Task<Response<ClientDto>> DeleteClientByIdAsync(int id);
+    }
+}
