fc764b3eb31fa67996b765e96f00fe1df0b4b5a8
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-05-25 18:19:40 +0300

    feat: Add StringValidation Utitlity class with common string validation methods
    - Added IsAllLetter method to check whether string consists only from letters or not.
    - Added IsPhoneNumber method to check whether string consists only from numbers or not.

---
+++ VCMS.Core/Utilities/StringValidations.cs
@@ -0,0 +1,35 @@
+﻿namespace VCMS.Core.Utilities
+{
+    public static class StringValidations
+    {
+        /// <summary>
+        /// Check if string consists only from letters or not.
+        /// </summary>
+        /// <param name="str">The string to check.</param>
+        /// <returns>A boolean indicating whether the word consists only of letters or not.</returns>
+        public static bool IsAllLetters(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            return str.All(c => char.IsLetter(c) || char.IsWhiteSpace(c));
+        }
+
+        /// <summary>
+        /// Check if string consists only from numbers or not.
+        /// </summary>
+        /// <param name="str">The string to check.</param>
+        /// <returns>A boolean indicating whether the word consists only of numbers or not.</returns>
+        public static bool IsPhoneNumber(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            return (str.All(c => char.IsNumber(c) || char.IsSymbol(c))) && (str.Length <= 15);
+        }
+    }
+}
