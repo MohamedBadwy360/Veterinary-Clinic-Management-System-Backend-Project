9c6ff267fabfd656c0fb864c327d9cdc48929179
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-05-30 22:13:30 +0300

    feat: Add MiddlewareExtensions class and rename RegisterServicesExtensions to DependenciesExtensions

--- VCMS.API/Extensions/DependenciesExtension.cs
+++ VCMS.API/Extensions/DependenciesExtension.cs
@@ -1,14 +1,17 @@
 ﻿namespace VCMS.API.Extensions
 {
-    public static class RegisterServicesExtension
+    public static class DependenciesExtension
     {
-        public static void RegisterServices(this IServiceCollection services)
+        public static void RegisterDependencies(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(SpeciesMappingProfile).Assembly);
+
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             services.AddScoped<ISpeciesService, SpeciesService>();
             services.AddScoped<IDiagnosisService, DiagnosisService>();
-            services.AddScoped<IClientService, ClientService>();
+            services.AddScoped<IClientService, ClientsService>();
+            services.AddScoped<IPatientService, PatientsService>();
         }
     }
 }
---
+++ VCMS.API/Extensions/MiddlewaresExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace VCMS.API.Extensions
+{
+    public static class MiddlewaresExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
