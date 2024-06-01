3876fb018465e6624cda1518dc6e4da1ff59dd87
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-01 18:14:44 +0300

    feat: add PatientsService to implement business logic related to patients

---
+++ VCMS.Core/Interfaces/ServicesInterfaces/IPatientsService.cs
@@ -0,0 +1,11 @@
+﻿namespace VCMS.Core.Interfaces.ServicesInterfaces
+{
+    public interface IPatientsService
+    {
+        Task<Response<GetPatientDto>> GetPatientByIdAsync(int id);
+        Task<Response<IEnumerable<GetPatientDto>>> GetAllPatientsAsync();
+        Task<Response<CreatePatientDto>> CreatePatientAsync(CreatePatientDto createPatientDto);
+        Task<Response<UpdatePatientDto>> UpdatePatientAsync(int id, UpdatePatientDto updatePatientDto);
+        Task<Response<DeletePatientDto>> DeletePatientAsync(int id);
+    }
+}
