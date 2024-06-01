f7486ea9885947224c7ad5f318d62ff59e317d18
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-01 18:15:16 +0300

    feat: add mapping profile for patients

---
+++ VCMS.Business/Mappings/PatientMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace VCMS.Business.Mappings
+{
+    public class PatientMappingProfile : Profile
+    {
+        public PatientMappingProfile()
+        {
+            CreateMap<Patient, GetPatientDto>()
+                .ForMember(dest => dest.ClientName, options => options.MapFrom(src => $"{src.Client.FirstName} {src.Client.LastName}"))
+                .ForMember(dest => dest.SpeciesName, options => options.MapFrom(src => src.Species.Name));
+
+            CreateMap<CreatePatientDto, Patient>();
+            CreateMap<UpdatePatientDto, Patient>();
+            CreateMap<Patient, DeletePatientDto>();
+        }
+    }
+}
