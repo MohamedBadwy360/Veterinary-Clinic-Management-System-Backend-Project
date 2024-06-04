15c8667769cb09da241987a733c8fb0bdc572a7a
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-04 14:16:01 +0300

    feat: add FindAsync and FindAllAsync methods overloads

--- VCMS.EF/Repositories/BaseRepository.cs
+++ VCMS.EF/Repositories/BaseRepository.cs
@@ -8,11 +8,56 @@
             _context = context;
         }
 
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes is not null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.ToListAsync();
+        }
 
         public async Task<T> AddAsync(T entity)
         {
             await _context.Set<T>().AddAsync(entity);
             return entity;
+        }
+
+        public T Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            return entity;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AnyAsync(predicate);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
         }
 
         public async Task<int> CountAsync()
@@ -25,14 +70,34 @@
             return await _context.Set<T>().CountAsync(predicate);
         }
 
-        public void Delete(T entity)
+        public async Task<T> FindAsync(Expression<Func<T, bool>> predicate,
+            string[] includes = null)
         {
-            _context.Set<T>().Remove(entity);
+            IQueryable<T> query = _context.Set<T>();
+            if (includes is not null)
+            {
+                foreach (string include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.SingleOrDefaultAsync(predicate);
         }
 
-        public void DeleteRange(IEnumerable<T> entities)
+        public async Task<T> FindAsync(Expression<Func<T, bool>> predicate,
+            params Expression<Func<T, object>>[] includes)
         {
-            _context.Set<T>().RemoveRange(entities);
+            IQueryable<T> query = _context.Set<T>();
+            if (includes is not null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.SingleOrDefaultAsync(predicate);
         }
 
         public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate,
@@ -51,14 +116,19 @@
             return await query.Where(predicate).ToListAsync();
         }
 
-        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate,
-            int skip, int take)
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes)
         {
-            return await _context.Set<T>()
-                .Where(predicate)
-                .Skip(skip)
-                .Take(take)
-                .ToListAsync();
+            IQueryable<T> query = _context.Set<T>();
+
+            if (includes is not null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.Where(predicate).ToListAsync();
         }
 
         public async Task<IEnumerable<T>> PaginateAsync(Expression<Func<T, bool>> predicate,
@@ -83,42 +153,6 @@
             }
 
             return await query.ToListAsync();
-        }
-
-        public async Task<T> FindAsync(Expression<Func<T, bool>> predicate, 
-            string[] includes = null)
-        {
-            IQueryable<T> query = _context.Set<T>();
-            if (includes is not null)
-            {
-                foreach (string include in includes)
-                {
-                    query = query.Include(include);
-                }
-            }
-
-            return await query.SingleOrDefaultAsync(predicate);
-        }
-
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            return await _context.Set<T>().ToListAsync();
-        }
-
-        public async Task<T> GetByIdAsync(int id)
-        {
-            return await _context.Set<T>().FindAsync(id);
-        }
-
-        public T Update(T entity)
-        {
-            _context.Set<T>().Update(entity);
-            return entity;
-        }
-
-        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
-        {
-            return await _context.Set<T>().AnyAsync(predicate);
         }
     }
 }
