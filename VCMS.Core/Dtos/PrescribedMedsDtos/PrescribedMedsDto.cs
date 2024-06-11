e344f0a0e9ad72df4fd81716fd8177ac0c3252a3
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-11 07:17:49 +0300

    feat: add dtos for data transfer related to PrescribedMeds

---
+++ VCMS.Core/Dtos/PrescribedMedsDtos/PrescribedMedsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace VCMS.Core.Dtos.PrescribedMedsDtos
+{
+    /// <summary>
+    /// Data taranfer object for prescibed medication
+    /// </summary>
+    public class PrescribedMedsDto
+    {
+        public int PrescriptionId { get; set; }
+        public int MedicationId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Frequency { get; set; }
+    }
+}
