34c56e127c821073eb13298cf6e4286f894b2fbe
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-11 19:34:31 +0300

    feat: add mapping profile for mapping between receipts and dtos

---
+++ VCMS.Business/Mappings/ReceiptMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace VCMS.Business.Mappings
+{
+    public class ReceiptMappingProfile : Profile
+    {
+        public ReceiptMappingProfile()
+        {
+            CreateMap<Receipt, GetReceiptDto>();
+            CreateMap<GetReceiptDto, Receipt>();
+
+            CreateMap<CreateReceiptDto, Receipt>();
+            CreateMap<UpdateReceiptDto, Receipt>();
+        }
+    }
+}
