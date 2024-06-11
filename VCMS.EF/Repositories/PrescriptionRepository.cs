901495efc212557748ec6ce1492850e3dc66a6f5
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-11 07:14:45 +0300

    feat: add PrescriptionResository to handle specific data logic related to prescriptions and replace it in UnitOfWork class

---
+++ VCMS.EF/Repositories/PrescriptionRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace VCMS.EF.Repositories
+{
+    public class PrescriptionRepository : BaseRepository<Prescription>, IPrescriptionRespository
+    {
+        private readonly VCMSDbContext _context;
+        public PrescriptionRepository(VCMSDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<GetPrescriptionDto>> GetAllPrescriptionsAsync()
+        {
+            return await _context.Prescriptions
+                .Include(p => p.PrescribedMeds)
+                .ThenInclude(pm => pm.Medication)
+                .Select(p => new GetPrescriptionDto
+                {
+                    CaseId = p.CaseId,
+                    Date = p.Date,
+                    PrescribedMedcations = p.PrescribedMeds.Select(pm => new GetPrescribedMedDto
+                    {
+                        MedicationName = pm.Medication.TradeName,
+                        Quantity = pm.Quantity,
+                        Price = pm.Price,
+                        Frequency = pm.Frequency,
+                        TotalPrice = pm.TotalPrice
+                    })
+                    .ToList()
+                })
+                .ToListAsync();
+        }
+    }
+}
