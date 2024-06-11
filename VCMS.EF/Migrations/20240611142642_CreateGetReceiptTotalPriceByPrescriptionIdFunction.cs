15444fce2a4f260dd5f5d2ff7c068b4277180243
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-11 19:30:28 +0300

    feat: adding precedure to get receipt's total  price by prescription id

---
+++ VCMS.EF/Migrations/20240611142642_CreateGetReceiptTotalPriceByPrescriptionIdFunction.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VCMS.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGetReceiptTotalPriceByPrescriptionIdFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                create function [dbo].[GetReceiptTotalPriceByPrescriptionId](@prescriptionId int)
+                returns decimal(8, 2)
+                as
+                begin 
+	                declare @receiptTotalPrice decimal(8, 2);
+
+	                select @receiptTotalPrice = sum(TotalPrice)
+	                from PrescribedMeds
+	                where PrescriptionId = @prescriptionId
+	                group by PrescriptionId;
+
+	                return @receiptTotalPrice;
+                end                       
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop function [dbo].[GetReceiptTotalPriceByPrescriptionId]");
+        }
+    }
+}
