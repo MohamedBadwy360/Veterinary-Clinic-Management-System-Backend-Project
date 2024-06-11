1880b5094efee57bf804e2040289d7552a31c0e9
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-11 07:17:23 +0300

    feat: add dtos for data transfer related to prescriptions

---
+++ VCMS.Core/Dtos/PrescriptionDtos/UpdatePrescriptionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VCMS.Core.Dtos.PrescriptionDtos
+{
+    /// <summary>
+    /// Data transfer object for updating prescription
+    /// </summary>
+    public class UpdatePrescriptionDto : PrescriptionDto
+    {
+    }
+}
