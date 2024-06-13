5919860fa51bfa8879020c0cda2b6b4bd45b7707
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-13 07:54:42 +0300

    feat: add AuthenticationController to handle authentication-related endpoints

---
+++ VCMS.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,43 @@
+﻿namespace VCMS.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController(IAuthService _authService) : ControllerBase
+    {
+        [SwaggerOperation(Summary = "Register a user")]
+        [ProducesResponseType(typeof(Response<AuthenticationDto>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response<AuthenticationDto>), StatusCodes.Status201Created)]
+        [ResponseCache(CacheProfileName = "Any-180")]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var response = await _authService.RegisterAsync(registerDto);
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+
+        [SwaggerOperation(Summary = "Login a user")]
+        [ProducesResponseType(typeof(Response<AuthenticationDto>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response<AuthenticationDto>), StatusCodes.Status200OK)]
+        [ResponseCache(CacheProfileName = "Any-180")]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var response = await _authService.LoginAsync(loginDto);
+            return StatusCode((int)response.StatusCode, response);
+        }
+
+
+        [SwaggerOperation(Summary = "Add role to a user")]
+        [ProducesResponseType(typeof(Response<AddRoleDto>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response<AddRoleDto>), StatusCodes.Status200OK)]
+        [ResponseCache(CacheProfileName = "NoCache")]
+        [Authorize(Roles = "Admin")]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddRole(AddRoleDto addRoleDto)
+        {
+            var response = await _authService.AddRoleAsync(addRoleDto);
+            return StatusCode((int)response.StatusCode, response);
+        }
+    }
+}
