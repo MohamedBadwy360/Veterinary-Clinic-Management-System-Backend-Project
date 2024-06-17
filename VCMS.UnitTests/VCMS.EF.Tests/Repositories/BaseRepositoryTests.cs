225023582df054475f6523b11d78ef89860034f9
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-17 09:25:47 +0300

    chore: clean namespaces

--- VCMS.UnitTests/VCMS.EF.Tests/Repositories/BaseRepositoryTests.cs
+++ VCMS.UnitTests/VCMS.EF.Tests/Repositories/BaseRepositoryTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq.Expressions;
-
-namespace VCMS.Tests.VCMS.EF.Tests.Repositories
+﻿namespace VCMS.Tests.VCMS.EF.Tests.Repositories
 {
     public class BaseRepositoryTests
     {
