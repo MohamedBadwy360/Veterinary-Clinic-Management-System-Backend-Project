f91d14b2e611af9ea7c6dd0a0fbdd10e25e62b06
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-27 12:55:06 +0300

    refactor: create readonly properities for all repositories and delete initialization from constructor
    - This help not initializing all repositories when creating an object of UnitOfWork

--- VCMS.EF/UnitOfWork/UnitOfWork.cs
+++ VCMS.EF/UnitOfWork/UnitOfWork.cs
@@ -3,36 +3,118 @@
     public class UnitOfWork : IUnitOfWork
     {
         private readonly VCMSDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IOptions<JwtOptions> _options;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private ICaseRepository _cases;
+        private IBaseRepository<Client> _clients;
+        private IBaseRepository<Diagnosis> _diagnostics;
+        private IBaseRepository<Doctor> _doctors;
+        private IBaseRepository<Medication> _medications;
+        private IPatientRepository _patients;
+        private IBaseRepository<PrescribedMeds> _prescribedMeds;
+        private IPrescriptionRespository _prescriptions;
+        private IReceiptRepository _receipts;
+        private IBaseRepository<Species> _species;
+        private IAuthRepository _authentication;
 
-        public UnitOfWork(VCMSDbContext context, UserManager<ApplicationUser> _user, 
-            IOptions<JwtOptions> _options, RoleManager<IdentityRole> _roleManager)
+        public UnitOfWork(VCMSDbContext context, UserManager<ApplicationUser> userManager,
+            IOptions<JwtOptions> options, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
-
-            Cases = new CaseRepository(_context);
-            Clients = new BaseRepository<Client>(_context);
-            Diagnostics = new BaseRepository<Diagnosis>(_context);
-            Doctors = new BaseRepository<Doctor>(_context);
-            Medications = new BaseRepository<Medication>(_context);
-            Patients = new PatientRepository(_context);
-            PrescribedMeds = new BaseRepository<PrescribedMeds>(_context);
-            Prescriptions = new PrescriptionRepository(_context);
-            Receipts = new ReceiptRepository(_context);
-            Species = new BaseRepository<Species>(_context);
-            Authentication = new AuthRepository(_user, _roleManager, _options);
+            _userManager = userManager;
+            _options = options;
+            _roleManager = roleManager;
         }
 
-        public ICaseRepository Cases { get; private set; }
-        public IBaseRepository<Client> Clients { get; private set; }
-        public IBaseRepository<Diagnosis> Diagnostics { get; private set; }
-        public IBaseRepository<Doctor> Doctors { get; private set; }
-        public IBaseRepository<Medication> Medications { get; private set; }
-        public IPatientRepository Patients { get; private set; }
-        public IBaseRepository<PrescribedMeds> PrescribedMeds { get; private set; }
-        public IPrescriptionRespository Prescriptions { get; private set; }
-        public IReceiptRepository Receipts { get; private set; }
-        public IBaseRepository<Species> Species { get; private set; }
-        public IAuthRepository Authentication { get; set; }
+        public ICaseRepository Cases
+        {
+            get
+            {
+                _cases ??= new CaseRepository(_context);
+                return _cases;
+            }
+        }
+        public IBaseRepository<Client> Clients
+        {
+            get
+            {
+                _clients ??= new BaseRepository<Client>(_context);
+                return _clients;
+            }
+        }
+        public IBaseRepository<Diagnosis> Diagnostics
+        {
+            get
+            {
+                _diagnostics ??= new BaseRepository<Diagnosis>(_context);
+                return _diagnostics;
+            }
+        }
+        public IBaseRepository<Doctor> Doctors
+        {
+            get
+            {
+                _doctors ??= new BaseRepository<Doctor>(_context);
+                return _doctors;
+            }
+        }
+        public IBaseRepository<Medication> Medications
+        {
+            get
+            {
+                _medications ??= new BaseRepository<Medication>(_context);
+                return _medications;
+            }
+        }
+        public IPatientRepository Patients
+        {
+            get
+            {
+                _patients ??= new PatientRepository(_context);
+                return _patients;
+            }
+        }
+        public IBaseRepository<PrescribedMeds> PrescribedMeds
+        {
+            get
+            {
+                _prescribedMeds ??= new BaseRepository<PrescribedMeds>(_context);
+                return _prescribedMeds;
+            }
+        }
+        public IPrescriptionRespository Prescriptions
+        {
+            get
+            {
+                _prescriptions ??= new PrescriptionRepository(_context);
+                return _prescriptions;
+            }
+        }
+        public IReceiptRepository Receipts
+        {
+            get
+            {
+                _receipts ??= new ReceiptRepository(_context);
+                return _receipts;
+            }
+        }
+        public IBaseRepository<Species> Species
+        {
+            get
+            {
+                _species ??= new BaseRepository<Species>(_context);
+                return _species;
+            }
+        }
+        public IAuthRepository Authentication
+        {
+            get
+            {
+                _authentication ??= new AuthRepository(_userManager, _roleManager, _options);
+                return _authentication;
+            }
+        }
 
         public async Task<int> CommitAsync()
         {
